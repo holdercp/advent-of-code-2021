3a7c89fda58e5972e52e0a75a1f581dba1150c0d
holdercp <25958393+holdercp@users.noreply.github.com>
2021-12-01 19:31:23 -0600

    Complete day 1, part 1

--- 01/part_1.py
+++ 01/part_1.py
@@ -1,5 +1,14 @@
-data = []
-with open("01/input.txt") as file:
-    data = file.read().splitlines()
+depths = []
+with open("01/input.txt") as f:
+    depths = f.read().splitlines()
 
-print(data)
+depths = [int(d) for d in depths]
+
+increases = 0
+prev = None
+for i, d in enumerate(depths):
+    if i > 0 and d > prev:
+        increases += 1
+    prev = d
+
+print(increases)
