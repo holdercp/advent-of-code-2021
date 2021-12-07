588731c095b79aad00e321d422ef5a782518d01b
holdercp <25958393+holdercp@users.noreply.github.com>
2021-12-07 09:24:29 -0600

    Refactor day 6, part 2

--- 06/part_2.py
+++ 06/part_2.py
@@ -7,38 +7,13 @@ with open("06/input.txt") as f:
 
 days = 256
 
+school = [0] * 9
+for f in fish:
+    school[f] += 1
 
-def generate_school(starting_fish):
-    school = {}
-    for n in range(9):
-        school[n] = 0
-
-    for f in starting_fish:
-        school[f] += 1
-
-    return school
-
-
-def age_fish(school):
-    new_fish = school[0]
-
-    school[0] = school[1]
-    school[1] = school[2]
-    school[2] = school[3]
-    school[3] = school[4]
-    school[4] = school[5]
-    school[5] = school[6]
-    school[6] = school[7]
-    school[7] = school[8]
-
-    school[8] = new_fish
-    school[6] += new_fish
-
-    return school
-
-
-school = generate_school(fish)
 for d in range(days):
-    school = age_fish(school.copy())
+    new_fish = reset_fish = school.pop(0)
+    school.append(new_fish)
+    school[6] += reset_fish
 
-print(sum(school.values()))
+print(sum(school))
