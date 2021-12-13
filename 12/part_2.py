4cb574b2e749e61f8f57f830666ee92ce8f20a88
holdercp <25958393+holdercp@users.noreply.github.com>
2021-12-12 21:35:05 -0600

    Complete day 12, part 2

---
+++ 12/part_2.py
@@ -0,0 +1,35 @@
+cave_graph = {}
+with open('12/input.txt') as f:
+    routes = f.read().strip().splitlines()
+    for route in routes:
+        start, end = route.split('-')
+        if start in cave_graph:
+            cave_graph[start].add(end)
+        else:
+            cave_graph[start] = {end}
+
+        if end in cave_graph:
+            cave_graph[end].add(start)
+        else:
+            cave_graph[end] = {start}
+
+
+def count_paths(graph, node, destination, visited=[], count=0):
+    if node == destination:
+        return 1
+    else:
+        visited.append(node)
+        lowers = [v for v in visited if v.islower() and v != 'start']
+        for n in graph[node]:
+            if n == 'start' or (n in lowers and len(lowers) > len(set(lowers))):
+                continue
+            else:
+                count += count_paths(graph=graph, node=n,
+                                     destination=destination, visited=visited.copy())
+        return count
+
+
+total = count_paths(graph=cave_graph, node='start',
+                    destination='end')
+
+print(total)
