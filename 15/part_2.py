09044dfaa4c2991c08071e5e0008bb008b01ff6c
holdercp <25958393+holdercp@users.noreply.github.com>
2021-12-16 22:26:40 -0600

    Finally complete day 15, part 2

--- 15/part_2.py
+++ 15/part_2.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 def increment_grid(grid):
     for y, row in enumerate(grid):
         for x, level in enumerate(row):
@@ -5,11 +8,8 @@ def increment_grid(grid):
     return grid
 
 
-risk_levels = {}
-grid_height = 0
-grid_width = 0
-with open('15/input.txt') as f:
-    grid = f.read().strip().splitlines()
+def create_grid(file):
+    grid = file.read().strip().splitlines()
     grid = [list(row) for row in grid]
 
     for i in range(len(grid) * 4):
@@ -21,16 +21,14 @@ with open('15/input.txt') as f:
         for i in range(len(row) * 4):
             row.append(int(row[i]) + 1 if int(row[i]) < 9 else 1)
 
+    return grid
+
+
+def get_neighbors(node, grid):
     grid_height = len(grid)
     grid_width = len(grid[0])
-    for y, row in enumerate(grid):
-        for x, level in enumerate(row):
-            location = (x, y)
-            risk_levels[location] = int(level)
 
-
-def get_unvisited_neighbors(node):
-    neighbors = set()
+    neighbors = {}
     x, y = node
 
     y_prev = y-1
@@ -43,50 +41,59 @@ def get_unvisited_neighbors(node):
     has_next_col = x_next < grid_width
 
     if has_prev_row:
-        node_key = (x, y_prev)
-        neighbors.add(node_key)
+        vertex = (x, y_prev)
+        neighbors[vertex] = int(grid[y_prev][x])
     if has_next_col:
-        node_key = (x_next, y)
-        neighbors.add(node_key)
+        vertex = (x_next, y)
+        neighbors[vertex] = int(grid[y][x_next])
     if has_next_row:
-        node_key = (x, y_next)
-        neighbors.add(node_key)
+        vertex = (x, y_next)
+        neighbors[vertex] = int(grid[y_next][x])
     if has_prev_col:
-        node_key = (x_prev, y)
-        neighbors.add(node_key)
+        vertex = (x_prev, y)
+        neighbors[vertex] = int(grid[y][x_prev])
 
-    unvisited_neighbors = neighbors.difference(visited)
-
-    return unvisited_neighbors
+    return neighbors
 
 
-def get_min_node():
-    unvisited = {node: level for node,
-                 level in risks.items() if node not in visited}
-    return min(unvisited, key=unvisited.get)
+def create_graph(grid):
+    graph = {}
+    for y, row in enumerate(grid):
+        for x, level in enumerate(row):
+            vertex = (x, y)
+            neighbors = get_neighbors(vertex, grid)
+            graph[vertex] = neighbors
+    return graph
 
 
-visited = set()
-risks = {}
+def calc_risk(graph, initial_vertex):
+    levels = {vertex: float('inf') for vertex in graph}
+    levels[initial_vertex] = 0
+    pq = [(0, initial_vertex)]
+
+    while len(pq) > 0:
+        current_level, current_vertex = heapq.heappop(pq)
+
+        if current_level > levels[current_vertex]:
+            continue
+
+        for neighbor, level in graph[current_vertex].items():
+            level = current_level + level
+
+            if level < levels[neighbor]:
+                levels[neighbor] = level
+                heapq.heappush(pq, (level, neighbor))
+
+    return levels
+
+
+levels_graph = {}
 initial = (0, 0)
-destination = (499, 499)
+target = (499, 499)
+with open('15/input.txt') as f:
+    grid = create_grid(f)
+    levels_graph = create_graph(grid)
 
-risks[initial] = 0
-while len(visited) < len(risk_levels):
-    current = get_min_node()
-    visited.add(current)
+risk = calc_risk(levels_graph, initial)
 
-    if current == destination:
-        break
-
-    neighbors = get_unvisited_neighbors(current)
-    for neighbor in neighbors:
-        level = risks[current] + risk_levels[neighbor]
-        if neighbor in risks:
-            if level < risks[neighbor]:
-                risks[neighbor] = level
-        else:
-            risks[neighbor] = level
-
-
-print(risks[destination])
+print(risk[target])
