9bb51708982d2267616f9942a3c7147526387a2a
holdercp <chrispholder@gmail.com>
2021-12-29 11:31:44 -0600

    Complete day 22, part 1

--- 22/part_1.py
+++ 22/part_1.py
@@ -3,17 +3,35 @@ with open('22/input.txt') as f:
     steps = f.read().strip().splitlines()
     for s in steps:
         step = {}
-        command, ranges = s.split(' ')
-        step['command'] = command
 
+        command, ranges = s.split(' ')
         ranges = ranges.split(',')
         for r in ranges:
-            coordinate, rnge = r.split('=')
-            start, end = rnge.split('..')
+            coordinate, rng = r.split('=')
+            start, end = rng.split('..')
+
+            if int(start) < -50 or 50 < int(end):
+                break
 
             step[coordinate] = {}
             step[coordinate] = range(int(start), int(end) + 1)
 
-        reboot_steps.append(step)
+        if step:
+            step['command'] = command
+            reboot_steps.append(step)
 
-print(reboot_steps)
+on = set()
+
+for s in reboot_steps:
+
+    for z in s['z']:
+        for y in s['y']:
+            for x in s['x']:
+                cuboid = (x, y, z)
+                if s['command'] == 'on':
+                    on.add(cuboid)
+                elif cuboid in on:
+                    on.remove(cuboid)
+
+
+print(len(on))
