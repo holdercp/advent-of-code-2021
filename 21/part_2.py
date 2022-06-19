c59e5abae8408f8ad7b36fb0d3678a687ac7d68e
holdercp <chrispholder@gmail.com>
2022-06-19 08:19:20 -0500

    Try caching

--- 21/part_2.py
+++ 21/part_2.py
@@ -1,6 +1,7 @@
 from collections import deque
 import copy
-from itertools import product, permutations
+from itertools import product
+from typing import Literal, TypedDict
 
 player_pos = []
 with open('21/input.txt') as f:
@@ -8,6 +9,17 @@ with open('21/input.txt') as f:
     for p in player_data:
         *_, pos = p.split(': ')
         player_pos.append(int(pos))
+
+player = TypedDict(
+    'Player', {'id': Literal['p1', 'p2'], 'score': int, 'pos': int})
+universe = TypedDict(
+    'Universe', {'p1': player, 'p2': player, 'current_player': player})
+
+
+def serializeUniverse(universe: universe, roll: int) -> str:
+    p1, p2, current_player = universe.values()
+    return f'{p1["id"]}:({p1["score"]},{p1["pos"]}),{p2["id"]}:({p2["score"]},{p2["pos"]}),cp:{current_player["id"]},m:{roll}'
+
 
 winning_score = 21
 rolls = [sum(p) for p in product([1, 2, 3], repeat=3)]
@@ -30,21 +42,27 @@ starting_state = {
     'p2': p2,
     'current_player': p1,
 }
-d = deque()
-d.append(starting_state)
+universes = deque()
+universes.append(starting_state)
 
-while len(d) > 0:
-    universe = d.pop()
+seenUniverses = set()
+
+while len(universes) > 0:
+    universe = universes.pop()
     for r in rolls:
         cp = universe['current_player']
         moves = ((cp['pos'] + r) % 10) or 10
+        universeState = serializeUniverse(universe=universe, roll=r)
 
-        if cp['score'] + moves >= winning_score:
+        if universeState in seenUniverses:
             wins[cp['id']] += 1
+        elif cp['score'] + moves >= winning_score:
+            wins[cp['id']] += 1
+            seenUniverses.add(universeState)
         else:
             next_u = copy.deepcopy(universe)
             next_u[cp['id']]['score'] += moves
             next_u[cp['id']]['pos'] = moves
             next_u['current_player'] = next_u['p1'] if next_u['current_player'] == next_u['p2'] else next_u['p2']
-            d.append(next_u)
+            universes.append(next_u)
 print(wins)
