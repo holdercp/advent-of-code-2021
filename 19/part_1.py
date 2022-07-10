4bee8b07b2482dace8b4f8dd05669c1c01023807
holdercp <25958393+holdercp@users.noreply.github.com>
2022-07-10 17:20:54 -0500

    Set up day 19

---
+++ 19/part_1.py
@@ -0,0 +1,11 @@
+scanner_data = []
+with open('19/input.txt') as f:
+    scanner_data_raw = f.read().strip().split('\n\n')
+    scanner_data_raw = [d.splitlines() for d in scanner_data_raw]
+    for sdr in scanner_data_raw:
+        coordinates = sdr[1:]
+        for c in coordinates:
+            scanner_data.append(tuple(c.split(",")))
+
+
+print(scanner_data)
