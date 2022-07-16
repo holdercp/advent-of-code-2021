798efe10da7b41231f5b3426859364377d082cbf
holdercp <25958393+holdercp@users.noreply.github.com>
2022-07-16 12:49:03 -0500

    Convert coordinate values to numbers

--- 19/part_1.py
+++ 19/part_1.py
@@ -4,8 +4,9 @@ with open('19/input.txt') as f:
     scanner_data_raw = [d.splitlines() for d in scanner_data_raw]
     for sdr in scanner_data_raw:
         coordinates = sdr[1:]
-        for c in coordinates:
-            scanner_data.append(tuple(c.split(",")))
+        for coords in coordinates:
+            scanner_data.append(tuple([int(coor)
+                                for coor in coords.split(",")]))
 
 
 print(scanner_data)
