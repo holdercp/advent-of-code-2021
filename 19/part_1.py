7c400031e1a0ee5f6b42398754e89d6799d80986
holdercp <25958393+holdercp@users.noreply.github.com>
2022-08-14 16:14:40 -0500

    Refactor original approach

--- 19/part_1.py
+++ 19/part_1.py
@@ -15,12 +15,13 @@ with open('19/input_2d.txt') as f:
 Coordinate = Tuple[int, int]
 BeaconList = List[Coordinate]
 
+OVERLAP_THRESHOLD = 3
+
 
 class Scanner:
     def __init__(self, id: str, beacons: BeaconList = []) -> None:
         self.id = id
         self.beacons = beacons
-        self.location = None
 
 
 ScannerList = List[Scanner]
@@ -29,7 +30,7 @@ ScannerList = List[Scanner]
 def setup() -> ScannerList:
     scanners = []
     for i, sd in enumerate(scanner_data):
-        scanner = Scanner(id=str(i), beacons=[b for b in sd])
+        scanner = Scanner(str(i), [b for b in sd])
         scanners.append(scanner)
     return scanners
 
@@ -39,8 +40,6 @@ def calc_offset(beacon_1: Coordinate, beacon_2: Coordinate) -> Coordinate:
 
 
 def check_overlap(scanner_1: Scanner, scanner_2: Scanner, offset: Coordinate) -> Tuple[bool, BeaconList]:
-    OVERLAP_THRESHOLD = 3
-
     offset_beacons: BeaconList = [
         (b[0] + offset[0], b[1] + offset[1]) for b in scanner_2.beacons]
     overlapping_beacon_locations: List[Coordinate] = set(
@@ -49,20 +48,31 @@ def check_overlap(scanner_1: Scanner, scanner_2: Scanner, offset: Coordinate) ->
     return (len(overlapping_beacon_locations) >= OVERLAP_THRESHOLD, offset_beacons)
 
 
+def search_overlap(origin_scanner: Scanner, scanners: ScannerList) -> Scanner:
+    for scanner in scanners:
+        for origin_beacon in origin_scanner.beacons:
+            for beacon in scanner.beacons:
+                offset = calc_offset(origin_beacon, beacon)
+                does_overlap, offset_beacons = check_overlap(
+                    origin_scanner, scanner, offset)
+
+                if does_overlap:
+                    scanner.beacons = offset_beacons
+                    return scanner
+
+    raise Exception(
+        f'Overlap was not found with scanner: {origin_scanner.id}')
+
+
 scanners = setup()
-current_scanner = scanners[0]
-beacons = set(current_scanner.beacons)
+scanner = scanners.pop(0)
+beacons = set(scanner.beacons)
 
-for scanner in scanners[1:]:
-    for current_beacon in current_scanner.beacons:
-        for beacon in scanner.beacons:
-            offset = calc_offset(beacon_1=current_beacon, beacon_2=beacon)
-            does_overlap, offset_beacons = check_overlap(
-                current_scanner, scanner, offset)
+while len(scanners) > 0:
+    overlapping_scanner = search_overlap(scanner, scanners)
+    beacons |= set(overlapping_scanner.beacons)
+    scanner = scanners.pop(scanners.index(overlapping_scanner))
 
-            if does_overlap:
-                scanner.location = offset
-                beacons |= set(offset_beacons)
-                break
 
 print(len(beacons))
+print(beacons)
