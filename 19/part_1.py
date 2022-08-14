2f4b3b1281e829599bfd2314838ca608cac6d167
holdercp <25958393+holdercp@users.noreply.github.com>
2022-08-14 15:16:00 -0500

    Clean up Beacon approach

--- 19/part_1.py
+++ 19/part_1.py
@@ -1,5 +1,4 @@
-from copy import deepcopy
-from typing import List, Tuple, TypedDict
+from typing import List, Tuple
 
 scanner_data = []
 with open('19/input_2d.txt') as f:
@@ -13,22 +12,8 @@ with open('19/input_2d.txt') as f:
                                   for coor in coords.split(",")]))
         scanner_data.append(beacons)
 
-Location = Tuple[int, int]
-Offset = TypedDict('Offset', {'x': int, 'y': int})
-
-
-class Beacon:
-    def __init__(self, location: Location) -> None:
-        self.x, self.y = location
-
-    def get_location(self) -> Location:
-        return (self.x, self.y)
-
-    def set_location(self, location: Location) -> None:
-        self.x, self.y = location
-
-
-BeaconList = List[Beacon]
+Coordinate = Tuple[int, int]
+BeaconList = List[Coordinate]
 
 
 class Scanner:
@@ -37,9 +22,6 @@ class Scanner:
         self.beacons = beacons
         self.location = None
 
-    def set_location(self, location: Location):
-        self.location = location
-
 
 ScannerList = List[Scanner]
 
@@ -47,47 +29,40 @@ ScannerList = List[Scanner]
 def setup() -> ScannerList:
     scanners = []
     for i, sd in enumerate(scanner_data):
-        beacon_list = []
-        for b in sd:
-            beacon_list.append(Beacon(b))
-        scanner = Scanner(id=str(i), beacons=beacon_list)
+        scanner = Scanner(id=str(i), beacons=[b for b in sd])
         scanners.append(scanner)
     return scanners
 
 
-def calc_offset(beacon_1: Beacon, beacon_2: Beacon) -> Offset:
-    return {
-        'x': beacon_1.x - beacon_2.x,
-        'y': beacon_1.y - beacon_2.y
-    }
+def calc_offset(beacon_1: Coordinate, beacon_2: Coordinate) -> Coordinate:
+    return (beacon_1[0] - beacon_2[0], beacon_1[1] - beacon_2[1])
 
 
-def check_overlap(scanner_1: Scanner, scanner_2: Scanner, offset: Offset) -> Tuple[bool, BeaconList]:
+def check_overlap(scanner_1: Scanner, scanner_2: Scanner, offset: Coordinate) -> Tuple[bool, BeaconList]:
     OVERLAP_THRESHOLD = 3
-    offset_beacons: BeaconList = [Beacon((b.x + offset['x'], b.y + offset['y']))
-                                  for b in scanner_2.beacons]
-    overlapping_beacon_locations: List[Location] = set([b.get_location() for b in scanner_1.beacons]).intersection(
-        b.get_location() for b in offset_beacons)
-    unique_beacon_locations = set([b.get_location() for b in scanner_1.beacons]).symmetric_difference(
-        b.get_location() for b in offset_beacons)
-    return (len(overlapping_beacon_locations) >= OVERLAP_THRESHOLD, [Beacon(l) for l in unique_beacon_locations])
+
+    offset_beacons: BeaconList = [
+        (b[0] + offset[0], b[1] + offset[1]) for b in scanner_2.beacons]
+    overlapping_beacon_locations: List[Coordinate] = set(
+        scanner_1.beacons).intersection(offset_beacons)
+
+    return (len(overlapping_beacon_locations) >= OVERLAP_THRESHOLD, offset_beacons)
 
 
 scanners = setup()
-
 current_scanner = scanners[0]
-master_scanner = Scanner(
-    id='master', beacons=deepcopy(current_scanner.beacons))
+beacons = set(current_scanner.beacons)
+
 for scanner in scanners[1:]:
     for current_beacon in current_scanner.beacons:
         for beacon in scanner.beacons:
             offset = calc_offset(beacon_1=current_beacon, beacon_2=beacon)
-            overlap_result = check_overlap(current_scanner, scanner, offset)
+            does_overlap, offset_beacons = check_overlap(
+                current_scanner, scanner, offset)
 
-            if overlap_result[0]:
-                scanner.set_location((offset['x'], offset['y']))
-                master_scanner.beacons = master_scanner.beacons + \
-                    overlap_result[1]
+            if does_overlap:
+                scanner.location = offset
+                beacons |= set(offset_beacons)
                 break
 
-print(len(master_scanner.beacons))
+print(len(beacons))
